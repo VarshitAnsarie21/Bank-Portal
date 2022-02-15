8782da6b927ab188b27694204900d5df54d62c1f
Devansh Galav <devanshgalav@gmail.com>
2022-02-15 19:35:23 +0530

    After login page

--- src/pages/AfterLogin/AfterAdminLoginPage.js
+++ src/pages/AfterLogin/AfterAdminLoginPage.js
@@ -3,6 +3,8 @@ import { Col, Typography, Input } from "antd";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import "./AfterAdminLoginPage.css";
+import { Table } from "react-bootstrap";
+import { button, ButtonToolbar } from "react-bootstrap";
 
 class AfterAdminLoginPage extends Component {
   constructor(props) {
@@ -12,7 +14,21 @@ class AfterAdminLoginPage extends Component {
   render() {
     return (
       <div className="after-admin-login-page">
-        
+        <div>
+          <Table className="mt-4" striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th colSpan={4}>Account No.</th>
+                <th colSpan={3}>Full Name</th>
+                <th colSpan={3}>Email Id</th>
+                <th colSpan={3}>Occupation</th>
+                <th colSpan={3}>Contact No.</th>
+                <th colSpan={3}>Permanent Address</th>
+                <th colSpan={3}>Options</th>
+              </tr>
+            </thead>
+          </Table>
+        </div>
       </div>
     );
   }
