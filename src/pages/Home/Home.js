ba7123dd4d1fec19b8bed051fc7f8947b3c58643
Devansh Galav <devanshgalav@gmail.com>
2022-02-25 21:34:49 +0530

    Final Update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ import "./Home.css";
 import Admin from "../../images/admin-logo.jpg";
 import Customer from "../../images/customer-logo.png";
 import "react-alice-carousel/lib/react-alice-carousel";
-import Header from "../../components/Header/Header"
+import Header from "../../components/Header/Header";
 import banner1 from "../../images/banner1.jpg";
 import banner2 from "../../images/banner2.jpeg";
 import banner3 from "../../images/banner3.jpeg";
@@ -24,8 +24,8 @@ let error = "";
 class Home extends Component {
   constructor(props) {
     super(props);
-    const token = sessionStorage.getItem("loggedUser")
-    const token1= sessionStorage.getItem("loggedAdmin")
+    const token = sessionStorage.getItem("loggedUser");
+    const token1 = sessionStorage.getItem("loggedAdmin");
 
     this.state = {
       index: 0,
@@ -40,7 +40,7 @@ class Home extends Component {
       adminCardErrorMessage: "",
       customerCardErrorMessage: "",
       user: token,
-      admin: token1
+      admin: token1,
     };
     this.adminLoginHandler = this.adminLoginHandler.bind(this);
     this.customerLoginHandler = this.customerLoginHandler.bind(this);
@@ -155,8 +155,8 @@ class Home extends Component {
               result.isSuccess === true ||
               result.message === "User Login successfully"
             ) {
-              sessionStorage.setItem("loggedUser", JSON.stringify(result))
-              this.setState({loggedIn: true})
+              sessionStorage.setItem("loggedUser", JSON.stringify(result));
+              this.setState({ loggedIn: true });
               this.props.history.push({
                 pathname: "/after-customer-login",
                 state: data,
@@ -197,8 +197,8 @@ class Home extends Component {
               result.isSuccess === true ||
               result.message === "Admin Login successfully"
             ) {
-              sessionStorage.setItem("loggedAdmin", JSON.stringify(result))
-              this.setState({loggedIn: true})
+              sessionStorage.setItem("loggedAdmin", JSON.stringify(result));
+              this.setState({ loggedIn: true });
               this.props.history.push({
                 pathname: "/after-admin-login",
                 state: data,
@@ -232,186 +232,196 @@ class Home extends Component {
       adminCardErrorMessage,
       customerCardErrorMessage,
     } = this.state;
-    if (this.state.user !== null && this.state.admin === null  && (email !== "" || email !== null || email !== undefined)) {
+    if (
+      this.state.user !== null &&
+      this.state.admin === null &&
+      (email !== "" || email !== null || email !== undefined)
+    ) {
       return <Redirect to="/after-customer-login" />;
-    } else if (this.state.user === null && this.state.admin !== null && (admin_email !== null || admin_email !== null || admin_email !== undefined)) {
+    } else if (
+      this.state.user === null &&
+      this.state.admin !== null &&
+      (admin_email !== null ||
+        admin_email !== null ||
+        admin_email !== undefined)
+    ) {
       return <Redirect to="/after-admin-login" />;
     } else {
       return (
         <React.Fragment>
-        <Header></Header>
-        <div className="home-page">
-          <Col>
-            <Row justify="center" className="home-page-inner-container">
-              <Col span={12} className="admin-col">
-                <Card variant="outlined" className="home-page-admin-card">
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={Admin}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      className="home-page-title"
-                    >
-                      Admin Login
-                    </Typography>
-                    <Input
-                      placeholder="Enter your Email ID"
-                      className="home-page-input"
-                      name="admin_email"
-                      value={admin_email}
-                      onChange={this.changeHandler}
+          <Header></Header>
+          <div className="home-page">
+            <Col>
+              <Row justify="center" className="home-page-inner-container">
+                <Col span={12} className="admin-col">
+                  <Card variant="outlined" className="home-page-admin-card">
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={Admin}
+                      alt="green iguana"
                     />
-                    <br />
-                    {adminEmailErrorMessage && (
-                      <div className="error-message-admin-div">
-                        {adminEmailErrorMessage}
-                      </div>
-                    )}
-                    {adminCardErrorMessage && !admin_email && (
-                      <div className="error-message-admin-div">
-                        {adminCardErrorMessage}
-                      </div>
-                    )}
-                    <Input
-                      placeholder="Enter your PassWord"
-                      className="home-page-input"
-                      value={admin_password}
-                      name="admin_password"
-                      onChange={this.changeHandler}
-                      type="password"
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        className="home-page-title"
+                      >
+                        Admin Login
+                      </Typography>
+                      <Input
+                        placeholder="Enter your Email ID"
+                        className="home-page-input"
+                        name="admin_email"
+                        value={admin_email}
+                        onChange={this.changeHandler}
+                      />
+                      <br />
+                      {adminEmailErrorMessage && (
+                        <div className="error-message-admin-div">
+                          {adminEmailErrorMessage}
+                        </div>
+                      )}
+                      {adminCardErrorMessage && !admin_email && (
+                        <div className="error-message-admin-div">
+                          {adminCardErrorMessage}
+                        </div>
+                      )}
+                      <Input
+                        placeholder="Enter your PassWord"
+                        className="home-page-input"
+                        value={admin_password}
+                        name="admin_password"
+                        onChange={this.changeHandler}
+                        type="password"
+                      />
+                      <br />
+                      {adminPasswordErrorMessage && (
+                        <div className="error-message-admin-div">
+                          {adminPasswordErrorMessage}
+                        </div>
+                      )}
+                      {adminCardErrorMessage && !admin_password && (
+                        <div className="error-message-admin-div">
+                          {adminCardErrorMessage}
+                        </div>
+                      )}
+                      <Button
+                        type="primary"
+                        className="home-page-login-button"
+                        onClick={this.adminLoginHandler}
+                      >
+                        Login
+                      </Button>
+                      <br />
+                    </CardContent>
+                  </Card>
+                </Col>
+                <Col span={12} className="customer-col">
+                  <Card variant="outlined" className="home-page-customer-card">
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={Customer}
+                      alt="green iguana"
                     />
-                    <br />
-                    {adminPasswordErrorMessage && (
-                      <div className="error-message-admin-div">
-                        {adminPasswordErrorMessage}
-                      </div>
-                    )}
-                    {adminCardErrorMessage && !admin_password && (
-                      <div className="error-message-admin-div">
-                        {adminCardErrorMessage}
-                      </div>
-                    )}
-                    <Button
-                      type="primary"
-                      className="home-page-login-button"
-                      onClick={this.adminLoginHandler}
-                    >
-                      Login
-                    </Button>
-                    <br />
-                  </CardContent>
-                </Card>
-              </Col>
-              <Col span={12} className="customer-col">
-                <Card variant="outlined" className="home-page-customer-card">
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={Customer}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      className="home-page-title"
-                    >
-                      Customer Login
-                    </Typography>
-                    <Input
-                      placeholder="Enter your Email ID"
-                      className="home-page-input"
-                      value={email}
-                      name="email"
-                      onChange={this.changeHandler}
-                    />
-                    <br />
-                    {CustomerEmailErrorMessage && (
-                      <div className="error-message-customer-div">
-                        {CustomerEmailErrorMessage}
-                      </div>
-                    )}
-                    {customerCardErrorMessage && !email && (
-                      <div className="error-message-customer-div">
-                        {customerCardErrorMessage}
-                      </div>
-                    )}
-                    <Input
-                      placeholder="Enter your PassWord"
-                      className="home-page-input"
-                      value={password}
-                      name="password"
-                      onChange={this.changeHandler}
-                      type="password"
-                    />
-                    <br />
-                    {customerPasswordErrorMessage && (
-                      <div className="error-message-customer-div">
-                        {customerPasswordErrorMessage}
-                      </div>
-                    )}
-                    {customerCardErrorMessage && !password && (
-                      <div className="error-message-customer-div">
-                        {customerCardErrorMessage}
-                      </div>
-                    )}
-                    <Button
-                      type="primary"
-                      className="home-page-login-button"
-                      onClick={this.customerLoginHandler}
-                    >
-                      Login
-                    </Button>
-                    <br />
-                    <Text className="home-page-text">
-                      Not a Registered Customer?
-                    </Text>{" "}
-                    <a className="home-page-hyperlink" href="/registration">
-                      Sign Up
-                    </a>
-                    <hr />
-                    <Text className="home-page-text">
-                      Reset Your PassWord!
-                    </Text>{" "}
-                    <a className="home-page-hyperlink" href="/reset-password">
-                      Here
-                    </a>
-                  </CardContent>
-                </Card>
-              </Col>
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        className="home-page-title"
+                      >
+                        Customer Login
+                      </Typography>
+                      <Input
+                        placeholder="Enter your Email ID"
+                        className="home-page-input"
+                        value={email}
+                        name="email"
+                        onChange={this.changeHandler}
+                      />
+                      <br />
+                      {CustomerEmailErrorMessage && (
+                        <div className="error-message-customer-div">
+                          {CustomerEmailErrorMessage}
+                        </div>
+                      )}
+                      {customerCardErrorMessage && !email && (
+                        <div className="error-message-customer-div">
+                          {customerCardErrorMessage}
+                        </div>
+                      )}
+                      <Input
+                        placeholder="Enter your PassWord"
+                        className="home-page-input"
+                        value={password}
+                        name="password"
+                        onChange={this.changeHandler}
+                        type="password"
+                      />
+                      <br />
+                      {customerPasswordErrorMessage && (
+                        <div className="error-message-customer-div">
+                          {customerPasswordErrorMessage}
+                        </div>
+                      )}
+                      {customerCardErrorMessage && !password && (
+                        <div className="error-message-customer-div">
+                          {customerCardErrorMessage}
+                        </div>
+                      )}
+                      <Button
+                        type="primary"
+                        className="home-page-login-button"
+                        onClick={this.customerLoginHandler}
+                      >
+                        Login
+                      </Button>
+                      <br />
+                      <Text className="home-page-text">
+                        Not a Registered Customer?
+                      </Text>{" "}
+                      <a className="home-page-hyperlink" href="/registration">
+                        Sign Up
+                      </a>
+                      <hr />
+                      <Text className="home-page-text">
+                        Reset Your PassWord!
+                      </Text>{" "}
+                      <a className="home-page-hyperlink" href="/reset-password">
+                        Here
+                      </a>
+                    </CardContent>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+            <Row>
+              <Carousel
+                className="home-carousel"
+                activeIndex={index}
+                onSelect={this.handleSelect}
+              >
+                <Carousel.Item>
+                  <img className="banner1" src={banner1} alt="First slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="banner2" src={banner2} alt="Second slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="banner3" src={banner3} alt="Third slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="banner4" src={banner4} alt="Fourth slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="banner5" src={banner5} alt="Fifth slide" />
+                </Carousel.Item>
+              </Carousel>
             </Row>
-          </Col>
-          <Row>
-            <Carousel
-              className="home-carousel"
-              activeIndex={index}
-              onSelect={this.handleSelect}
-            >
-              <Carousel.Item>
-                <img className="banner1" src={banner1} alt="First slide" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img className="banner2" src={banner2} alt="Second slide" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img className="banner3" src={banner3} alt="Third slide" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img className="banner4" src={banner4} alt="Fourth slide" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img className="banner5" src={banner5} alt="Fifth slide" />
-              </Carousel.Item>
-            </Carousel>
-          </Row>
-        </div>
+          </div>
         </React.Fragment>
       );
     }
